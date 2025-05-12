66246a3ee4ac21eaac1a544fc871bd36b08181f9
stubanx <gantavyaoo@gmail.com>
2025-05-12 20:17:51 +0530

    modified:   app/components/ConsolidateSection/page.tsx 	new file:   app/favicon.ico 	modified:   app/globals.css 	modified:   app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,10 @@ import TestSection from "./components/TestSection/page";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-  
+    <div className="items-center justify-items-center font-[family-name:var(--font-geist-sans)]">
+  <TestSection/>
       <ConsolidateSection  />
+  <TestSection/>
 
     </div>
   );
