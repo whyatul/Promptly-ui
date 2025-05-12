0668acc50ae34a2de98d919f6a9364f216e5f83b
manisha0rrmy <manisha9177264555@gmail.com>
2025-05-12 21:31:20 +0530

    completed creating generative-ai-visual

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
 import Image from "../public/next.svg";
 import ConsolidateSection from "./components/ConsolidateSection/page";
 import TestSection from "./components/TestSection/page";
-
+import GenerativeAIVisual from "./components/custom ai/generative-ai-visual";
 export default function Home() {
   return (
     <div className="items-center justify-items-center p-8 font-[family-name:var(--font-geist-sans)]">
   <TestSection/>
       <ConsolidateSection  />
   <TestSection/>
-
+<GenerativeAIVisual/>
     </div>
   );
 }
